e9d57d7836749cfcec0869b157b8fddc5fab82a3
clean-command-1851 <ed7901979t@gmail.com>
2019-08-01 17:32:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
 def reverse_each_word(array)
   return_array[]
   array.each   do |string|
-  return_array.
+  return_array << string.reverse
 end
+letters["do", "re", "mi"]
+reverse_each_word(letters)
