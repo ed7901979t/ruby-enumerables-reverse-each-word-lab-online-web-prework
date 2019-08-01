fd09ea74f85abb832822c9bcb284d749440a8fef
clean-command-1851 <ed7901979t@gmail.com>
2019-08-01 17:42:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,13 @@ def reverse_each_word(array)
   return_array[]
   array.each   do |string|
   return_array << string.reverse
+end 
 end
 
 def reverse_each_word(array)
   return_array[]
   array.collect |string|
   return_array << string.reverse
+end
 end
 
