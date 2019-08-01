5e804dc6639ab8c9d213b3f53994292d220cf02b
clean-command-1851 <ed7901979t@gmail.com>
2019-08-01 18:50:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,4 +10,15 @@ def reverse_each_word(string)
   end
   return_array.join(" ")
 end
+
+
+def reverse_each_word(string)
+  
+  array = string.split(" ")
+  return_array = []
+  array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
  
